a7c0dae99b1b800f7e4f23d84d0fb9f9a33e22af
MSouch <msouch3@gmail.com>
2023-10-23 14:47:07 -0600

    Cleaned up comments/ommited uneccesary stuff

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+# open file and read data within file
 with open("mydefaults.ini.txt") as ini_file:
     data = ini_file.read()
 
 # initialize variable for lower and uppercase letter counts as 0
 lowercase_count = 0
 uppercase_count = 0
+
+# initialize variable for numbers in file 
 number_count = 0
 
+#split data in file 
 lines = data.split("\n")
 key_value_ini = dict()
+
 for line in lines:
     if "=" in line:
         fields = line.split("=")
-        #key_value_ini[fields[0]] = fields[1]
-# iterate through the characters in each line
     for char in line:
         # counter for if character is lowercase
         if char.islower():
@@ -24,7 +27,7 @@ for line in lines:
         if number.isnumeric():
             number_count += 1
 # print the ouput
-print (f" there are {number_count} number(s) in the file")
+print (f"There are {number_count} number(s) in the file")
 print (f"There are {uppercase_count} uppercase letters in the file")
 print (f"There are {lowercase_count} lowercase letters in the file")
 #print(key_value_ini)
